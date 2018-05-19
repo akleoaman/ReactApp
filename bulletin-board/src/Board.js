91fea3ac2130dfc1b430de6dbef8d4481d7e2adc
Amandeep <akleoaman@gmail.com>
2018-05-19 17:50:21 +0200

    7.6 Add Multiple Notes
    Using Add function we can add multiple notes on Board

--- bulletin-board/src/Board.js
+++ bulletin-board/src/Board.js
@@ -1,30 +1,37 @@
 import React, { Component } from 'react'
 import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
 
 class Board extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            notes: 
-            [
-                {
-                    id: 0,
-                    note: "call Mummy"
-                },
-                {
-                    id: 1,
-                    note: "Message Anju di"
-                },
-                {
-                    id: 2,
-                    note: "Learn React"
-                }
-            ]
+            notes: []
         }
         this.eachNote = this.eachNote.bind(this)
         this.update = this.update.bind(this)
         this.remove = this.remove.bind(this)
+        this.add = this.add.bind(this)
+        this.nextId = this.nextId.bind(this)
 
+    }
+
+    add(text){
+        this.setState(prevState => ({
+            notes: [
+                ...prevState.notes,
+                {
+                    id: 3,
+                    note: text
+                }
+            ]
+        }))
+    }
+
+    nextId()
+    {
+        this.uniqueId = this.uniqueId || 0
+        return this.uniqueId++
     }
     update(newText, i)
     {
@@ -59,7 +66,8 @@ class Board extends Component {
             return(
                 <div className="board">
                   {this.state.notes.map(this.eachNote)}
-               
+                  <button onClick={this.add.bind(null, "New Note")}  id="add"><FaPlus />
+                  </button>
                 </div>
             )
         }
